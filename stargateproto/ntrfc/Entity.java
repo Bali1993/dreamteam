ad3e30d2ac8bca3c11508953402a5c88fe826550
Matt9413 <hidasi.matyi@gmail.com>
2016-04-09 15:02:26 +0200

    Added files via upload

--- stargateproto/ntrfc/Entity.java
+++ stargateproto/ntrfc/Entity.java
@@ -3,17 +3,29 @@ package ntrfc;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+import game.Character;
 import game.Replicator;
 
 public interface Entity {
-	/*
-	 * deklarálja a getRec() függvényt, hogy a listában tárolt elemeken meghívhassuk
-	 */
+	
+	//deklarálja a getRec() függvényt, hogy a listában tárolt elemeken meghívhassuk
 	public abstract Rectangle getRec();
 	
-	public void onCollisionWithReplicator(int dx, int dy, Replicator replicator);
-	public void onCollisionWithBullet(int i);
-	public void onCollision(int dx, int dy, int i, game.Character character);
+	//elsõ paraméter
+	//		azt adjuk meg, hogy Repliator, Character vagy Bullet - el történt az ütközés
+	//CollisionIndexinListofElements (korábban "i" paraméter volt)
+	//		a listában mely indexen történt az ütközés
+	//		rájöttem erre a paraméterre nincs is szükség, mert az adott elem, amivel történik az ütközés
+	//		tudja a saját indexét a listában, nem kell neki átadni
+	//		csak simán lekérdezi indexOf(this) beépített függvénnyel
+	//dx, dy
+	//		az azt adja, meg hogy mely irányból történt az ütközés
+	//		0, -32, +32 lehet az értéke
+	//		leginább arra használjuk, hogy az adott elem lepattintsa magáról a Replicatort vagy Charactert
+	//		ezért nincs rá szükség a Bullet-nél
+	public void onCollisionWithReplicator(Replicator replicator, int dx, int dy);
+	public void onCollisionWithBullet(game.Bullet bullet);
+	public void onCollisionWithCharacter(Character character, int dx, int dy);
 	public void render(Graphics g);
 		
 }
